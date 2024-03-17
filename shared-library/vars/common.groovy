9f63ceacbf2a0805cfa5750ccaade6a544fff438
s poorna chandra <poornanaidu1@gmail.com>
2024-03-17 16:05:50 +0530

    commit

--- shared-library/vars/common.groovy
+++ shared-library/vars/common.groovy
@@ -39,12 +39,31 @@ def test() {
 
 def codeQuality() {
     stage('Code Quality') {
-            print 'OK'
+        env.sonaruser = sh (script: 'aws ssm get-parameter --name "sonarqube.user" --with-decryption --query="Parameter.Value" |xargs', returnStdout: true).trim()
+        env.sonarpass = sh (script: 'aws ssm get-parameter --name "sonarqube.pass" --with-decryption --query="Parameter.Value" |xargs', returnStdout: true).trim()
+        wrap([$class: "MaskPasswordsBuildWrapper", varPasswordPairs: [[password: sonarpass]]]) {
+            if(env.codeType == "maven") {
+                //sh 'sonar-scanner -Dsonar.host.url=http://172.31.89.117:9000 -Dsonar.login=${sonaruser} -Dsonar.password=${sonarpass} -Dsonar.projectKey=${component} -Dsonar.qualitygate.wait=true -Dsonar.java.binaries=./target'
+                print 'OK'
+            }else {
+                //sh 'sonar-scanner -Dsonar.host.url=http://172.31.89.117:9000 -Dsonar.login=${sonaruser} -Dsonar.password=${sonarpass} -Dsonar.projectKey=${component} -Dsonar.qualitygate.wait=true'
+                print 'OK'
+            }
+        }
+    }
+}
+
+def codeSecurity() {
+    stage('Code Security') {
+        print 'Code Security'
+
+        // IN code security we will generally used SAST & SCA checks
+        // You can say that your company is using checkmarx sast and checkmarx sca for this,
     }
 }
 
 def release() {
     stage('release') {
-        print 'release'
+        print 'release '
     }
 }
